a2cd66bf1fe1ec23923ca0f4b2846ba80871b8c6
Vojislav Stevanović <95634547+pipstur@users.noreply.github.com>
2023-07-23 16:58:39 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+# app.py (Backend server)
+
+from flask import Flask, request, jsonify
+import openai
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app, resources={r"/get_pokemon_info": {"origins": "http://localhost:8000"}})
+
+openai.api_key = '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'
+
+@app.route('/get_pokemon_info', methods=['POST'])
+def get_pokemon_info():
+    data = request.get_json()
+    user_input = data['text'] + "which pokemon am I?Answer in a freindly tone :). Keep it concise and under 200 words. Format the answer like this: \nName of the pokemon: \nDescription: \nReasoning: "
+
+    # Use the ChatGPT API to interact with the model
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": user_input}
+        ],
+        temperature=0.2,  
+        max_tokens=200   
+    )
+
+    response_text = response['choices'][0]['message']['content']
+    name_start = "Name of the pokemon:"
+    description_start = "Description:"
+    reasoning_start = "Reasoning:"
+
+    name_index = response_text.find(name_start)
+    description_index = response_text.find(description_start)
+    reasoning_index = response_text.find(reasoning_start)
+
+    if name_index == -1 or description_index == -1 or reasoning_index == -1:
+        # If any of the keywords is missing, it means the input is not identifiable as a Pokémon
+        response_data = {
+            'pokemon_name': "None",
+            'pokemon_description': "None",
+            'reasoning': "None",
+            'pokemon_image_url': "https://via.placeholder.com/150"   # You can add the image URL here later
+        }
+    else:
+        # Extract name, description, and reasoning as before
+        
+        name = response_text[name_index + len(name_start):description_index].strip()
+        description = response_text[description_index + len(description_start):reasoning_index].strip()
+        reasoning = response_text[reasoning_index + len(reasoning_start):].strip()
+        pokeapi_url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
+        pokeapi_response = requests.get(pokeapi_url)
+        if pokeapi_response.status_code == 200:
+            pokemon_data = pokeapi_response.json()
+            pokemon_image_url = pokemon_data['sprites']['front_default']
+        else:
+        # If the Pokemon data cannot be fetched, set default values
+            description = "No description available."
+            pokemon_image_url = "https://via.placeholder.com/150"  # Placeholder image URL
+
+        # Prepare the response data
+        response_data = {
+            'pokemon_name': name,
+            'pokemon_description': description,
+            'reasoning': reasoning,
+            'pokemon_image_url': pokemon_image_url  
+        }
+
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000, debug=True)
